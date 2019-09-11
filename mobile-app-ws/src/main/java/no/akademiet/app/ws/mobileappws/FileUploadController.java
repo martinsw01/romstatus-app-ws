51f716a338cd0f83078a6d5fc23ec5a5c1a08e77
martinsw01 <martin.s.winther@gmail.com>
2019-09-11 16:52:30 +0200

    Added dynamic website to server

--- mobile-app-ws/src/main/java/no/akademiet/app/ws/mobileappws/FileUploadController.java
+++ mobile-app-ws/src/main/java/no/akademiet/app/ws/mobileappws/FileUploadController.java
@@ -7,12 +7,27 @@ import no.akademiet.app.ws.mobileappws.tests.FileTest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 @RestController
 public class FileUploadController {
 
     @GetMapping(path = "/object")
     public Rooms getRooms() {
         return new Rooms(XmlToObject.convertXml());
+    }
+
+    @GetMapping(path = "/romstatus")
+    public String getHtml() {
+        try {
+            String html = new String(Files.readAllBytes(Paths.get("/home/pi/romstatus-WS/romstatus-app-ws/mobile-app-ws/src/main/resources/static/index.html")));
+            System.out.println(html);
+            return html;
+        }
+        catch (Exception e) {
+            return "";
+        }
     }
 
 }
